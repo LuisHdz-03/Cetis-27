9714d23956c22235ab11de051d3067b03b75bf8f
Luis Hernandez <hegl030420@itsuruapan.edu.mx>
2025-12-03 20:11:19 -0600

    creo que ya

--- constants/api.ts
+++ constants/api.ts
@@ -1,4 +1,4 @@
 // constants/api.ts
 // Configuración de la URL base de la API
 
-export const API_BASE_URL = "https://tu-backend.up.railway.app"; // Reemplaza con la URL real de Railway
+export const API_BASE_URL = 'https://cetis-27-production.up.railway.app';
